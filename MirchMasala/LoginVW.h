3d846d2ef934c6681c3198e64e0e4d1dad4883c4
Mango SW <jigneshbs@outlook.com>
2017-03-04 19:13:38 +0530

    commit by jignesh

---
+++ MirchMasala/LoginVW.h
@@ -0,0 +1,13 @@
+//
+//  LoginVW.h
+//  MirchMasala
+//
+//  Created by Mango SW on 04/03/2017.
+//  Copyright © 2017 jkinfoway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVW : UIViewController
+
+@end
